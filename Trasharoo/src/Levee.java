9ffaba5c67c6d776f0bd362d777ad6648e027ce1
Sasorishi <34128056+Sasorishi@users.noreply.github.com>
2018-04-27 21:38:43 +0200

    Update Levee.java

--- Trasharoo/src/Levee.java
+++ Trasharoo/src/Levee.java
@@ -1,45 +1,47 @@
 import java.util.Scanner;
 
-public class Levee {
-
+public class Levee 
+{
 	private int numero;
 	private String dateLevee;
 	private float poids;
 	
-	public Levee() {
+	public Levee() 
+	{
 		this.numero = 0;
-		this.dateLevee="";
-		this.poids=0;
-		
+		this.dateLevee = "";
+		this.poids = 0;	
 	}
 	
-	
-	public void  levee() {}
-	public void  saisir() {
+	public void saisir() 
+	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Saisissez le numero de la levée");
-		this.numero=sc.nextInt();
 		
-		System.out.println("Saisissez la date  de la levée");
-		this.dateLevee=sc.next();
+		System.out.println("Saisissez le numero de la levÃ©e");
+		this.numero = sc.nextInt();
 		
-		System.out.println("Saisissez le poids de la levée");
-		this.poids=sc.nextFloat();
+		System.out.println("Saisissez la date de la levÃ©e");
+		this.dateLevee = sc.next();
+		
+		System.out.println("Saisissez le poids de la levÃ©e");
+		this.poids = sc.nextFloat();
 	}
 	
-	public void  afficher() {
-		System.out.println("Le numero de la levée est :"+this.numero);
-		System.out.println("La date de la levée est :"+this.dateLevee);
-		System.out.println("La poids de la levée est :"+this.poids);
+	public void afficher() 
+	{
+		System.out.println("Le numero de la levÃ©e est :" +this.numero);
+		System.out.println("La date de la levÃ©e est :" +this.dateLevee);
+		System.out.println("La poids de la levÃ©e est :" +this.poids);
 	}
 	
-	public String toXml() {
-		String chaine; 
-		chaine ="<Levee>";
-		chaine += "\t<numero>"+this.numero+"</numero>\n";
-		chaine += "\t<datelevee>"+this.dateLevee+"</datelevee>\n";
-		chaine += "\t<poids>"+this.poids+"</poids>\n";
-		chaine +="</Levee>\n";
+	public String toXml() 
+	{
+		String chaine = ""; 
+		chaine = "<levee>";
+		chaine += "\t<numero>" + this.numero + "<numero>\n";
+		chaine += "\t<datelevee>" + this.dateLevee + "<datelevee>\n";
+		chaine += "\t<poids>" + this.poids + "<poids>\n";
+		chaine += "<levee>\n";
 		return chaine ;
 	}
 
@@ -72,9 +74,5 @@ public class Levee {
 	public void setPoids(float poids) {
 		this.poids = poids;
 	}
-	
-	
-	
-	
-	
 }
+

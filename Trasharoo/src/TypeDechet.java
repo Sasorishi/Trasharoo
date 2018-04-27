c2d0a8fce3bfaffc59a61dcb9e1d491e5d22d30f
Sasorishi <34128056+Sasorishi@users.noreply.github.com>
2018-04-27 21:40:17 +0200

    Update TypeDechet.java

--- Trasharoo/src/TypeDechet.java
+++ Trasharoo/src/TypeDechet.java
@@ -1,51 +1,86 @@
 import java.util.Scanner;
 
-public class TypeDechet {
-	
-	
+public class TypeDechet 
+{
 	private int code;
 	private String libelle;
 	private String recyclable;
 	private float tarif ;
-	public TypeDechet() {
-		this.code=0;
-		this.libelle="";
-		this.recyclable ="";
-		this.tarif=0;
-
-	}
-	public void TypeDechet() {}
-	public void saisir() {
-		Scanner sc = new Scanner (System.in);
-		System.out.println("Saississez le code du type de Dechet :");
-		this.code =sc.nextInt();
-		
-		System.out.println("Saississez le libelle du type Dechet :");
-		this.libelle =sc.next();
-		
-		System.out.println("Le dechet est-il recyclable ? :");
-		this.recyclable =sc.next();
-		
-		System.out.println("Saississez le tarif du Dechet :");
-		this.tarif =sc.nextFloat();
-	}
-	public void afficher() {
-		System.out.println("Le code du dechet est :"+this.code);
-		System.out.println("Le libelle du dechet est :"+this.libelle);
-		System.out.println("Recyclable ?"+this.recyclable);
-		System.out.println("Tarif du dechet :"+this.tarif+"€");
-	}
-	public String toXml() {
-		String chaine ="";
-		chaine ="<typeDechet> ";
-		chaine +="\t<code>"+this.code+"</code>\n";
-		chaine += "\t<libelle>" + this.libelle + "</civilitee>\n";
-		chaine += "\t<recyclable>" + this.recyclable + "</recyclable>\n";
-		chaine += "\t<tarif>" + this.tarif + "</tarif>\n";
-		chaine +="</typeDechet> ";
-		return chaine;
+	
+	public TypeDechet() 
+	{
+		this.code = 0;
+		this.libelle = "";
+		this.recyclable = "";
+		this.tarif = 0;
 	}
 	
-	public void menu() {}
+	public void saisir() 
+	{
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Saississez le code du type de Dechet");
+		this.code = sc.nextInt();
+		
+		System.out.println("Saississez le libelle du type Dechet");
+		this.libelle = sc.next();
+		
+		System.out.println("Le dechet est-il recyclable ?");
+		this.recyclable = sc.next();
+		
+		System.out.println("Saississez le tarif du Dechet");
+		this.tarif = sc.nextFloat();
+	}
+	
+	public void afficher() 
+	{
+		System.out.println("Le code du dechet est : " +this.code);
+		System.out.println("Le libelle du dechet est : " +this.libelle);
+		System.out.println("Recyclable : " +this.recyclable);
+		System.out.println("Tarif du dechet : " +this.tarif+ "â‚¬");
+	}
+	
+	public String toXml() 
+	{
+		String chaine ="";
+		chaine = "<typeDechet> ";
+		chaine += "\t<code>" + this.code + "<code>\n";
+		chaine += "\t<libelle>" + this.libelle + "<libelle>\n";
+		chaine += "\t<recyclable>" + this.recyclable + "<recyclable>\n";
+		chaine += "\t<tarif>" + this.tarif + "<tarif>\n";
+		chaine += "<typeDechet> ";
+		return chaine;
+	}
 
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public String getRecyclable() {
+		return recyclable;
+	}
+
+	public void setRecyclable(String recyclable) {
+		this.recyclable = recyclable;
+	}
+
+	public float getTarif() {
+		return tarif;
+	}
+
+	public void setTarif(float tarif) {
+		this.tarif = tarif;
+	}
 }

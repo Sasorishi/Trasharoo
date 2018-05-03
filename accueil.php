69125f88d2ac092cb30e7dff58f05e24209be7c3
Sasorishi <34128056+Sasorishi@users.noreply.github.com>
2018-05-03 14:30:15 +0200

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,248 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+    <head>
+        <title>INTRANET - POUBELLE</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="description" content="Template by Colorlib" />
+        <meta name="keywords" content="HTML, CSS, JavaScript, PHP" />
+        <meta name="author" content="Colorlib" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <link rel="shortcut icon" href="images/favicon.png" />
+        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700%7CLibre+Baskerville:400,400italic,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css"  href='css/clear.css' />
+        <link rel="stylesheet" type="text/css"  href='css/common.css' />
+        <link rel="stylesheet" type="text/css"  href='css/font-awesome.min.css' />
+        <link rel="stylesheet" type="text/css"  href='css/carouFredSel.css' />
+        <link rel="stylesheet" type="text/css"  href='css/sm-clean.css' />
+        <link rel="stylesheet" type="text/css"  href='style.css' />
+
+        <!--[if lt IE 9]>
+                <script src="js/html5.js"></script>
+        <![endif]-->
+
+    </head>
+
+
+    <body class="home blog">
+
+        <!-- Preloader Gif -->
+        <table class="doc-loader">
+            <tbody>
+                <tr>
+                    <td>
+                        <img src="images/ajax-document-loader.gif" alt="Loading...">
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <!-- Left Sidebar -->
+        <div id="sidebar" class="sidebar">
+            <div class="menu-left-part">
+                <div class="search-holder">
+                    <label>
+                        <input type="search" class="search-field" placeholder="Type here to search..." value="" name="s" title="Search for:">
+                    </label>
+                </div>
+                <div class="site-info-holder">
+                    <h1 class="site-title">INTRANET</h1>
+                    <p class="site-description">
+                        Gestion des poubelles de l'usager
+                    </p>
+                </div>
+                <nav id="header-main-menu">
+                    <ul class="main-menu sm sm-clean">
+                        <li><a href="accueil.php" class="current">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="scroll.php">Scroll</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </nav>
+                <footer>
+                    <div class="footer-info">
+                        © 2018 SUPPABLOG HTML TEMPLATE. <br> CRAFTED WITH <i class="fa fa-heart"></i> BY <a href="https://colorlib.com">COLORLIB</a>.
+                    </div>
+                </footer>
+            </div>
+            <div class="menu-right-part">
+                <div class="logo-holder">
+                    <a href="accueil.php">
+                        <img src="images/logo.png" alt="Suppablog WP">
+                    </a>
+                </div>
+                <div class="toggle-holder">
+                    <div id="toggle">
+                        <div class="menu-line"></div>
+                    </div>
+                </div>
+                <div class="social-holder">
+                    <div class="social-list">
+                        <a href="meshabitations.php?page=1"><i class="fa fa-twitter"><br/>Mes habitations</i></a>
+                        <a href="mespoubelles.php?page=1"><i class="fa fa-youtube-play"><br/>Mes poubelles</i></a>
+                        <a href="leslevees.php?page=1"><i class="fa fa-facebook"><br/>Les levées</i></a>
+                        <a href="lestypesdechets.php?page=1"><i class="fa fa-vimeo"><br/>Les types de déchets</i></a>
+                    </div>
+                </div>
+                <div class="fixed scroll-top"><i class="fa fa-caret-square-o-up" aria-hidden="true"></i></div>
+            </div>
+            <div class="clear"></div>
+        </div>
+
+        <!-- Home Content -->
+        <div id="content" class="site-content">
+            <div id="blog-wrapper">
+                <div class="blog-holder center-relative">
+
+
+                    <article id="post-1" class="blog-item-holder featured-post">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <div class="cat-links">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Crafting</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="entry-date published">February 12, 2016</div>
+                                <h2 class="entry-title">
+                                    <a href="single.html">Whatever is begun in anger ends in shame</a>
+                                </h2>
+                                <div class="excerpt">
+                                    Now when I had mastered the language of this water and had come to know every trifling feature that bordered the great river as familiarly as I knew the letters of the alphabet, I had made a valuable acquisition. I still keep in mind a certain wonderful sunset which I witnessed when and steamboating<a class="read-more" href="single.html"></a>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+
+
+                    <article id="post-2" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="single.html">I like to reinvent myself</a>
+                                </h2>
+                                <div class="cat-links">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Science</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="entry-date published">February 12, 2016</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+
+
+                    <article id="post-3" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="single.html">Everything is design</a>
+                                </h2>
+                                <div class="cat-links">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Art</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="entry-date published">February 12, 2016</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+
+
+
+                    <article id="post-4" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="single.html">It’s all about experience</a>
+                                </h2>
+                                <div class="cat-links">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Life</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="entry-date published">February 12, 2016</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+
+
+
+                    <article id="post-5" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="single.html">This is not my code</a>
+                                </h2>
+                                <div class="cat-links">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Coding</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="entry-date published">February 12, 2016</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+
+
+
+                    <article id="post-6" class="blog-item-holder">
+                        <div class="entry-content relative">
+                            <div class="content-1170 center-relative">
+                                <h2 class="entry-title">
+                                    <a href="single.html">Support human activity</a>
+                                </h2>
+                                <div class="cat-links">
+                                    <ul>
+                                        <li>
+                                            <a href="#">Advocacy</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="entry-date published">February 12, 2016</div>
+                                <div class="clear"></div>
+                            </div>
+                        </div>
+                    </article>
+
+                </div>
+                <div class="clear"></div>
+                <div class="block load-more-holder">LOAD MORE ENTRIES</div>
+            </div>
+
+            <div class="featured-image-holder">
+                <div class="featured-post-image" style="background-image: url(images/accueil2.png)"></div>
+
+            </div>
+            <div class="clear"></div>
+        </div>
+
+
+
+        <!--Load JavaScript-->
+        <script type="text/javascript" src="js/jquery.js"></script>
+        <script type='text/javascript' src='js/imagesloaded.pkgd.js'></script>
+        <script type='text/javascript' src='js/jquery.nicescroll.min.js'></script>
+        <script type='text/javascript' src='js/jquery.smartmenus.min.js'></script>
+        <script type='text/javascript' src='js/jquery.carouFredSel-6.0.0-packed.js'></script>
+        <script type='text/javascript' src='js/jquery.mousewheel.min.js'></script>
+        <script type='text/javascript' src='js/jquery.touchSwipe.min.js'></script>
+        <script type='text/javascript' src='js/jquery.easing.1.3.js'></script>
+        <script type='text/javascript' src='js/main.js'></script>
+    </body>
+</html>
